55dd9193682a4ca3d3071379ba5c050fe71fcbbf
chrisfender0@gmail.com <chrisfender0@gmail.com>
2020-03-05 14:32:49 -0800

    Update Controller

--- RubberDuckin/src/com/cova/Controller.java
+++ RubberDuckin/src/com/cova/Controller.java
@@ -3,25 +3,50 @@ package com.cova;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.util.Properties;
 
 public class Controller {
 	
 	private Model model;
 	private View view;
+	String url = "jdbc:mysql://127.0.0.1:3306/rubberduckin";
+	String user = "default";
+	String pass = "cova";
+	Connection conn = null;
+	PreparedStatement pstmt = null;
+	ResultSet rset = null;
+	ResultSetMetaData rsmd = null;
 	
 	public Controller(Model model, View view) {
 		this.model = model;
 		this.view = view;
 		try {
 			taskManager();
-		} catch (IOException e) {
+		} catch (IOException | SQLException e) {
 			e.printStackTrace();
 		}
 	}
 	
-	private void taskManager() throws IOException {
+	private void initConnection() throws SQLException {
+		Properties props = new Properties();
+		props.put("username", user);
+		props.put("password", pass);
+		conn = DriverManager.getConnection(url, "default", "cova");
+		conn.setAutoCommit(false);
+	}
+	
+	private void taskManager() throws IOException, SQLException {
 		view.menuOptions();
+		initConnection();
 		processUserInput();
+		closeResources();
+		closeConnection();
 	}
 	
 	private String getUserInput() throws IOException {
@@ -31,24 +56,86 @@ public class Controller {
 		return userInput;
 	}
 	
-	private void processUserInput() throws IOException {
+	private void processUserInput() throws IOException, SQLException {
 		int userInput = Integer.parseInt(getUserInput());
 		switch (userInput) {
 		case 1: {
-			model.selectAllFromProduct();
+			executeSimpleQuery(model.selectAllFromProduct());
+			showResults();
+			closeResources();
 			break;
 		}
 		case 2:{
-			model.selectAllFromOrders();
+			executeSimpleQuery(model.selectAllFromOrders());
+			showResults();
+			closeResources();
 			break;
 		}
 		case 3:{
-			model.selectAllFromCustomers();
+			executeSimpleQuery(model.selectAllFromCustomers());
+			showResults();
+			closeResources();
 			break;
 		}
-		default: break;
-			
+		default: 
+			break;
 		}
+	}
+	
+	private void executeSimpleQuery(String str) throws SQLException {
+		pstmt = conn.prepareStatement(str);
+		pstmt.execute();
+		rset = pstmt.getResultSet();
+		rsmd = rset.getMetaData();
+	}
+	
+	private StringBuilder showResults() throws SQLException {
+		int columnCount = rsmd.getColumnCount();
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append("\n");
+		
+		while(rset.next()) {
+			for(int i=1; i<columnCount; i++) {
+				sb.append(rsmd.getColumnName(i) + "\t");
+				sb.append(rset.getString(i));
+				sb.append("\n");
+			}
+			sb.append("\n");
+		}
+		
+		return sb;
+		
+	}
+	
+	private void showResultsTableFormat() throws SQLException {
+		int columnCount = rsmd.getColumnCount();
+		StringBuilder sb = new StringBuilder();
+		
+		for(int i=1; i<columnCount; i++) {
+			sb.append(rsmd.getColumnName(i) + "\t\t");
+		}
+		
+		sb.append("\n*****************************************************************\n");
+		
+		while(rset.next()) {
+			for(int i=1; i<columnCount; i++) {
+				sb.append(String.format("%-5s\t\t", rset.getString(i)));
+			}
+			sb.append("\n");
+		}
+		
+		System.out.println(sb);
+		
+	}
+	
+	private void closeConnection() throws SQLException {
+		conn.close();
+	}
+	
+	private void closeResources() throws SQLException {
+		pstmt.close();
+		rset.close();
 	}
 	
 }
