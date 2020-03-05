075c4cefb852e90b815ef587794a9a805427cb2c
chrisfender0@gmail.com <chrisfender0@gmail.com>
2020-03-05 15:46:38 -0800

    test for romy

--- RubberDuckin/src/com/cova/Main.java
+++ RubberDuckin/src/com/cova/Main.java
@@ -5,5 +5,6 @@ public class Main {
 	public static void main(String[] args) {
 		new Controller(new Model(), new View());
 	}
-//shove a comment in there
+//shove a comment in there 
+	//can you see this ? 
 }
