e67a81bb0f0131a9fdb70118d20471c5aac04295
chrisfender0@gmail.com <chrisfender0@gmail.com>
2020-03-05 16:43:33 -0800

    master commit

--- RubberDuckin/src/com/cova/Main.java
+++ RubberDuckin/src/com/cova/Main.java
@@ -2,16 +2,10 @@ package com.cova;
 
 public class Main { 
 
-<<<<<<< HEAD
-	public static void main(String[] args) {
-		new Controller(new Model(), new View());
-=======
 	public static void main(String[] args) {
 		new Controller(new Model(), new View());
 	}
-	
 	public void unnecessaryMethod() {
 		//please push 
->>>>>>> refs/remotes/origin/Chris
 	}
 }
