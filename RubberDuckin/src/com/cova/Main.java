5e8bd64fe4af88af324bd20d341433c60f1dbfb5
Zachary Michaels <zachary.michaels@outlook.com>
2020-03-05 16:02:02 -0800

    test2

--- RubberDuckin/src/com/cova/Main.java
+++ RubberDuckin/src/com/cova/Main.java
@@ -5,6 +5,6 @@ public class Main {
 	public static void main(String[] args) {
 		new Controller(new Model(), new View());
 	}
-//shove a comment in there 
+//shove a comment in there zach
 	//can you see this ? - yes i can ...
 }
