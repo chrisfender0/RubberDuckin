7a6864002a9cd592c5ac55da5a8e61235566d858
chrisfender0@gmail.com <chrisfender0@gmail.com>
2020-03-05 16:39:14 -0800

    unnecessary commit Chris

--- RubberDuckin/src/com/cova/Main.java
+++ RubberDuckin/src/com/cova/Main.java
@@ -5,5 +5,8 @@ public class Main {
 	public static void main(String[] args) {
 		new Controller(new Model(), new View());
 	}
-	//from Chris
+	
+	public void unnecessaryMethod() {
+		//please push 
+	}
 }
