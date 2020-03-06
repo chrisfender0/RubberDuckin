84f905526cbd0a083b2b02141c863e55b194b7fa
chrisfender0@gmail.com <chrisfender0@gmail.com>
2020-03-06 13:40:30 -0800

    quick fix

--- RubberDuckin/src/com/cova/Main.java
+++ RubberDuckin/src/com/cova/Main.java
@@ -4,10 +4,6 @@ public class Main {
 
 	public static void main(String[] args) {
 		new Controller(new Model(), new View());
-<<<<<<< HEAD
 	}
-	
-=======
-	}
->>>>>>> branch 'master' of https://github.com/chrisfender0/RubberDuckin.git
+
 }
