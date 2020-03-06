ad6daa2afa5f3abb2a9ec8746fb498c16a625ac1
chrisfender0@gmail.com <chrisfender0@gmail.com>
2020-03-05 16:51:55 -0800

    ....

--- RubberDuckin/src/com/cova/Main.java
+++ RubberDuckin/src/com/cova/Main.java
@@ -5,12 +5,6 @@ public class Main {
 	public static void main(String[] args) {
 		new Controller(new Model(), new View());
 	}
-<<<<<<< HEAD
-	public void unnecessaryMethod() {
-		//please push 
-	}
-=======
 	
 	
->>>>>>> refs/remotes/origin/Chris
 }
